e5fccbcac195e06ce3c602bffab869fe92d24248
Yu Sun <usunyu@gmail.com>
2017-09-21 19:12:01 +0800

    Initial Animation Engine

---
+++ animation_engine/Assets/AnimationEngine/AnimationComponent.cs
@@ -0,0 +1,189 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Animations;
+using UnityEngine.Audio;
+
+namespace AnimationEngine
+{
+    /// <summary>
+    /// Animation Component.
+    /// Example usage:
+    /// /* Initial */
+    /// AnimationComponent component = 
+    /// new AnimationComponent(mixer, animation, audio)
+    ///      .SetStartBlendingTime()
+    ///      .Start();
+    ///
+    /// /* Update */
+    /// component.SetEndBlendingTime().Tick();
+    /// </summary>
+    public class AnimationComponent
+    {
+        public enum Status
+        {
+            Pending,
+            Processing,
+            Done
+        }
+
+        private AnimationMixerPlayable animationMixer;
+        private AudioMixerPlayable audioMixer;
+        private int animationIndex;
+        private int audioIndex;
+
+        private float speed = 1.0f;
+
+        //                  totalTime
+        // +----------+--------------------+----------+
+        //   startBlendingTime      endBlendingTime
+        private float startBlendingTime = -1.0f;
+        private float endBlendingTime = 99999.0f;
+        private float startBlendingStep = -1.0f;
+        private float endBlendingStep = -1.0f;
+
+        private float processingTime = 0.0f;
+        private float totalTime = 0.0f;
+        private float weight = 0.0f;
+
+        private bool canStartNextClip = false;
+
+        private Status status = Status.Pending;
+
+        private const float DEFAULT_START_BLENDING_PERCENTAGE = 0.25f;
+        private const float DEFAULT_END_BLENDING_PERCENTAGE = 0.75f;
+
+        public AnimationComponent(AnimationMixerPlayable animationMixer, AudioMixerPlayable audioMixer, int animation, int audio = -1)
+        {
+            this.animationMixer = animationMixer;
+            this.audioMixer = audioMixer;
+            this.animationIndex = animation;
+            this.audioIndex = audio;
+
+            var clip = (AnimationClipPlayable)animationMixer.GetInput(animationIndex);
+            // Calculate time for processing current clip.
+            totalTime = clip.GetAnimationClip().length;
+        }
+
+        public void Reset()
+        {
+            startBlendingStep = -1.0f;
+            endBlendingTime = 99999.0f;
+            status = Status.Pending;
+        }
+
+        public void Start()
+        {
+            if (status != Status.Pending)
+            {
+                return;
+            }
+
+            var clip = (AnimationClipPlayable)animationMixer.GetInput(animationIndex);
+            // Reset the time so that the clip starts at the correct position.
+            clip.SetTime(0);
+            // Reverse the animation if necessary.
+            clip.SetSpeed(speed);
+
+            if (startBlendingTime <= 0.0f)
+            {
+                // Move weight to 1 directly.
+                animationMixer.SetInputWeight(animationIndex, 1.0f);
+            }
+            else
+            {
+                // Calculate blending step.
+                startBlendingStep = 1.0f / startBlendingTime;
+            }
+
+            status = Status.Processing;
+        }
+
+        public void Tick()
+        {
+            if (status != Status.Processing)
+            {
+                return;
+            }
+
+            processingTime += Time.deltaTime;
+
+            if (processingTime > endBlendingTime)
+            {
+                if (endBlendingStep <= 0.0f)
+                {
+                    endBlendingStep = 1.0f / (totalTime - endBlendingTime);
+                }
+                if (!canStartNextClip)
+                {
+                    canStartNextClip = true;
+                }
+                weight -= endBlendingStep;
+            }
+            else if (processingTime < startBlendingTime)
+            {
+                weight += startBlendingStep;
+            }
+
+            animationMixer.SetInputWeight(animationIndex, weight);
+
+            if (processingTime >= totalTime)
+            {
+                // Make sure we clean current animation clip.
+                animationMixer.SetInputWeight(animationIndex, 0.0f);
+                status = Status.Done;
+            }
+        }
+
+        public AnimationComponent SetAnimationSpeed(float speed = 1.0f)
+        {
+            this.speed = speed;
+            return this;
+        }
+
+        public AnimationComponent SetStartBlendingTime(float blendingTime = -1.0f)
+        {
+            if (blendingTime > 0.0f)
+            {
+                this.startBlendingTime = blendingTime;
+            }
+            else
+            {
+                this.startBlendingTime = totalTime * DEFAULT_START_BLENDING_PERCENTAGE;
+            }
+            return this;
+        }
+
+        public AnimationComponent SetEndBlendingTime(float blendingTime = -1.0f)
+        {
+            if (blendingTime > 0.0f)
+            {
+                this.endBlendingTime = blendingTime;
+            }
+            else
+            {
+                this.endBlendingTime = totalTime * DEFAULT_END_BLENDING_PERCENTAGE;
+            }
+            return this;
+        }
+
+        public int Animation { get { return animationIndex; } }
+
+        public int Audio { get { return audioIndex; } }
+
+        public float Speed { get { return speed; } }
+
+        public Status CurrentStatus { get { return status; } }
+
+        public bool CanStartNextClip { get { return canStartNextClip; } }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "[AnimationComponent: Animation={0}, Audio={1}, Speed={2}, Status={3}]",
+                animationIndex,
+                audioIndex,
+                speed,
+                status);
+        }
+    }
+}
---
+++ animation_engine/Assets/AnimationEngine/AnimationEngine.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Animations;
+using UnityEngine.Audio;
+
+namespace AnimationEngine
+{
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(AudioSource))]
+    public class AnimationEngine : MonoBehaviour
+    {
+        // Playable graph which controlling animations & audios.
+        PlayableGraph playableGraph;
+
+        // All animation clip for this engine.
+        public AnimationClip[] animationClips;
+
+        // All audio clip for this engine.
+        public AudioClip[] audioClips;
+
+        // Animation mixer for switching animation.
+        private AnimationMixerPlayable animationMixer;
+
+        // Audio mixer for switching audio.
+        private AudioMixerPlayable audioMixer;
+
+        // Animation component list will be play.
+        private List<AnimationComponent> currentAnimationList;
+
+        // Current playing animation component.
+        private AnimationComponent firstAnimation = null;
+
+        // Next playing animation component.
+        private AnimationComponent secondAnimation = null;
+
+        // Tag for debugging.
+        private const string TAG = "[AnimationEngine]";
+
+        // Use this for initialization
+        private void Start()
+        {
+            // Create playable graph.
+            playableGraph = PlayableGraph.Create();
+
+            // Create playable output node.
+            var playableOutput = AnimationPlayableOutput.Create(playableGraph, "Animation", GetComponent<Animator>());
+
+            // Create animation mixer playerable node.
+            int animationClipsLength = animationClips == null ? 0 : animationClips.Length;
+            animationMixer = AnimationMixerPlayable.Create(playableGraph, animationClipsLength);
+
+            // Connect the mixer to an output.
+            playableOutput.SetSourcePlayable(animationMixer);
+
+            // Create clip playable node for all animations.
+            for (int i = 0; i < animationClipsLength; i++)
+            {
+                // Wrap the clip in a playable.
+                var clipPlayable = AnimationClipPlayable.Create(playableGraph, animationClips[i]);
+
+                playableGraph.Connect(clipPlayable, 0, animationMixer, i);
+                animationMixer.SetInputWeight(i, 0.0f);
+            }
+
+            // Create audio output node.
+            var audioOutput = AudioPlayableOutput.Create(playableGraph, "Audio", GetComponent<AudioSource>());
+
+            // Create audio mixer playerable node.
+            int audioClipsLength = audioClips == null ? 0 : audioClips.Length;
+            audioMixer = AudioMixerPlayable.Create(playableGraph, audioClipsLength);
+
+            // Connect the mixer to an output.
+            audioOutput.SetSourcePlayable(audioMixer);
+
+            // Create clip playable node for all audios.
+            for (int i = 0; i < audioClipsLength; i++)
+            {
+                // Wrap the clip in a playable.
+                var clipPlayable = AudioClipPlayable.Create(playableGraph, audioClips[i], false);
+
+                playableGraph.Connect(clipPlayable, 0, audioMixer, i);
+                audioMixer.SetInputWeight(i, 0.0f);
+            }
+
+            // Init the animation component list.
+            currentAnimationList = new List<AnimationComponent>();
+
+            // Plays the playable graph.
+            playableGraph.Play();
+
+            // Register graph visual client to show debug messages.
+            // GraphVisualizerClient.Show(playableGraph, "Animation Engine Graph");
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (animationMixer.GetInputCount() == 0 || currentAnimationList.Count == 0)
+            {
+                // No valid clip input.
+                return;
+            }
+
+            if (firstAnimation != null)
+            {
+                switch (firstAnimation.CurrentStatus)
+                {
+                    case AnimationComponent.Status.Pending:
+                        firstAnimation.Start();
+                        break;
+                    case AnimationComponent.Status.Processing:
+                        firstAnimation.Tick();
+                        break;
+                    case AnimationComponent.Status.Done:
+                        if (currentAnimationList.Count == 1)
+                        {
+                            firstAnimation.Reset();
+                        }
+                        else if (currentAnimationList.Count >= 2)
+                        {
+                            currentAnimationList.Remove(firstAnimation);
+                            firstAnimation = currentAnimationList[0];
+                            if (currentAnimationList.Count >= 2)
+                            {
+                                secondAnimation = currentAnimationList[1];
+                            }
+                            else
+                            {
+                                secondAnimation = null;
+                            }
+                        }
+                        break;
+                }
+                // We can start blend next clip.
+                if (firstAnimation.CanStartNextClip)
+                {
+                    switch (secondAnimation.CurrentStatus)
+                    {
+                        case AnimationComponent.Status.Pending:
+                            secondAnimation.Start();
+                            break;
+                        case AnimationComponent.Status.Processing:
+                            secondAnimation.Tick();
+                            break;
+                        case AnimationComponent.Status.Done:
+                            // Impossibile
+                            break;
+                    }
+                }
+            }
+        }
+
+        // OnDisable is called when the behaviour becomes disabled () or inactive
+        private void OnDisable()
+        {
+            // Destroys all Playables and Outputs created by the graph.
+            playableGraph.Destroy();
+        }
+
+        // Add clip to pending animation list.
+        public void AddAnimations(int[] animations, int[] audios = null, bool queued = true)
+        {
+            if (!queued)
+            {
+                CleanPendingAnimations();
+            }
+            for (int index = 0; index < animations.Length; index++)
+            {
+                int audioIndex = -1;
+                if (audios != null && index < audios.Length)
+                {
+                    audioIndex = audios[index];
+                }
+                AnimationComponent component = new AnimationComponent(animationMixer, audioMixer, index, audioIndex);
+                currentAnimationList.Add(component);
+            }
+        }
+
+        // Clean current animation list in pending state.
+        private void CleanPendingAnimations()
+        {
+            if (currentAnimationList.Count < 2)
+            {
+                return;
+            }
+            currentAnimationList.RemoveRange(1, currentAnimationList.Count - 1);
+        }
+    }
+}
